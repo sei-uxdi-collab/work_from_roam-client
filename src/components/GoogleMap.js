bd436abe8921159005b5fae84f488b59391627d5
yingsongsana <yingsongsana@gmail.com>
2020-02-12 15:32:31 -0500

    Used geolocation to locate `userLocation`
    Not entirely most accurate location(getting info from network routing
    address as opposed to internal GPS)
    
    https://developers.google.com/maps/documentation/javascript/geolocation

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -3,18 +3,35 @@ import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react'
 import TestComponent from './TestComponent'
 
 class GoogleMap extends React.Component {
-    constructor(props) {
-        super(props)
+    constructor() {
+        super()
         this.state = {
             showWindow: false,
-            selectedMarker: null
+            showMarker: false,
+            selectedMarker: null,
+            userLocation: ''
         }
     }
+
+    // Using geolocation from browser to location user location
+    componentDidMount = () => {
+      if(navigator && navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(pos => {
+        console.log('found user location')
+        const coords = pos.coords
+        const lat = coords.latitude
+        const lng = coords.longitude
+        this.setState({ userLocation: { lat, lng },
+                        showMarker: true })
+                      })
+      }
+    }
+
     // onClick handler to set marker to state and show corresponding info window
     onMarkerClick = (props, marker, event) => {
         this.setState({ selectedMarker: marker, showWindow: true })
     }
-    
+
     // onClose handler for InfoWindow
     onInfoWindowClose = () => {
         this.setState({ showWindow: false })
@@ -28,11 +45,11 @@ class GoogleMap extends React.Component {
              zoom={14}
              clickableIcons={true}
             >
-            
-            {/* Marker needs a position prop to render, initially undefined 
+
+            {/* Marker needs a position prop to render, initially undefined
                 User search sets the coordinates and passed down as props.coordinates */}
             <Marker onClick={this.onMarkerClick}
-                  position={this.props.coordinates}
+                  position={this.state.userLocation}
                   name={'Current location'}
             />
 
