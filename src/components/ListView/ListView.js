1d5133f131afab693fa966c262f43fe4d1528fe0
chardbres <richard.m.bresnahan@gmail.com>
2020-03-21 16:40:52 -0400

    Addded initial code for ListView functionality. Need to figure out how to get it to display on the application, on all views.

--- src/components/ListView/ListView.js
+++ src/components/ListView/ListView.js
@@ -1,19 +1,30 @@
 import React from 'react';
+<<<<<<< HEAD
 import './ListView.scss'
+=======
+>>>>>>> Addded initial code for ListView functionality. Need to figure out how to get it to display on the application, on all views.
 
 class ListView extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
+<<<<<<< HEAD
       worskspaceList: '',
       expanded: false
+=======
+      worskspaceList: ''
+>>>>>>> Addded initial code for ListView functionality. Need to figure out how to get it to display on the application, on all views.
     }
   }
 
   render () {
     return (
       <div className='list-view'>
+<<<<<<< HEAD
         <p>List View</p>
+=======
+        <h3>List View</h3>
+>>>>>>> Addded initial code for ListView functionality. Need to figure out how to get it to display on the application, on all views.
       </div>
     )
   }
