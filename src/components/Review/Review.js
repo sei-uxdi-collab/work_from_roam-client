b4315bb32f5ae43213a13b08a1cffcc0df875fce
yingsongsana <yingsongsana@gmail.com>
2020-03-25 13:50:55 -0400

    Added placeholder pic for user profile photo

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -10,10 +10,16 @@ class Review extends React.Component {
 
     // render information inside an infoWindow for POI
     render() {
+      // Placeholder pic for now. Can either use limited set of avatars later or user-uploaded photo
+      // depending on how we want to implement the user profile feature
+      let userPic = 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'
         return (
-            <div style={{ borderStyle: 'double'}}>
-            <p>{this.props.user}: Stars here{this.props.rating}</p>
-              <p>{this.props.note}</p>
+            <div>
+            <img alt='User profile pic' src={userPic} style={{ width: '60px', height: '60px', borderRadius:'10px', float: 'left'}}/>
+            <p>{this.props.user}   Stars here({this.props.rating})</p>
+            <div style={{ padding: '5px' }}>
+            <p>{this.props.note}</p>
+            </div>
             </div>
         )
     }
