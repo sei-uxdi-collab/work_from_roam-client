3c1cc4cb7c736743a7d58192cf867cd9982833b6
yingsongsana <yingsongsana@gmail.com>
2020-03-26 15:57:12 -0400

    Added `StarRating` component to user reviews
    *Styling for the `StarRating` component currently an inline styling
    inside `Review.js` file to avoid merge conflict.
    SHOULD BE MOVED TO `ReviewForm.scss`!!!!

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { StarRating } from './StarsRating'
 
 class Review extends React.Component {
     constructor(props) {
@@ -15,10 +16,26 @@ class Review extends React.Component {
       let userPic = 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'
         return (
             <div>
-            <img alt='User profile pic' src={userPic} style={{ width: '60px', height: '60px', borderRadius:'10px', float: 'left'}}/>
-            <p>{this.props.user}   Stars here({this.props.rating})</p>
-            <div style={{ padding: '5px' }}>
+            <img alt='User profile pic' src={userPic} style={{ width: '60px', height: '60px', borderRadius:'10px', float: 'left' }}/>
+            <div style={{ float: 'right', paddingRight: '105px' }}>
+            <StarRating
+              value={this.props.rating}
+              emptyStarColor={'#4775FF'}
+              editing={false}
+            />
+            </div>
+            <span style={{ padding: '10px', margin: '10px', fontSize: '16px', lineHeight: '150%' }}>{this.props.user}</span>
+            <div style={{ position: 'static',
+                          padding: '10px 10px 10px 80px',
+                          margin: '1px',
+                          height: '60px',
+                          left: '0px',
+                          top: '32px',
+                          fontWeight: '300',
+                          fontSize: '13px',
+                          lineHeight: '150%' }}>
             <p>{this.props.note}</p>
+
             </div>
             </div>
         )
