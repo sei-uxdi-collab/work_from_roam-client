2b8f95cf0d9d017b386f057e24aaff90f60e04f4
asooge <asooge@gmail.com>
2020-03-31 19:22:17 -0400

    Updates

---
+++ precache-manifest.542c6e143ba5cc93eef46f40e0e3d5d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27c6db4fe639517de39dee7ea00f2bc8",
+    "url": "/work_from_roam-client/index.html"
+  },
+  {
+    "revision": "63e686a81d14a3cecc9f",
+    "url": "/work_from_roam-client/static/css/main.aa36e8db.chunk.css"
+  },
+  {
+    "revision": "731f8913e937d4272f2f",
+    "url": "/work_from_roam-client/static/js/2.dc341d16.chunk.js"
+  },
+  {
+    "revision": "2b8cc04d3b239d52c1e995484cbcd9e3",
+    "url": "/work_from_roam-client/static/js/2.dc341d16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63e686a81d14a3cecc9f",
+    "url": "/work_from_roam-client/static/js/main.d24bab41.chunk.js"
+  },
+  {
+    "revision": "691df9043ad91b2ff65d",
+    "url": "/work_from_roam-client/static/js/runtime-main.632891d8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/work_from_roam-client/precache-manifest.10c8fad78ef7788ebebf2dc60aeedd9c.js"
+  "/work_from_roam-client/precache-manifest.542c6e143ba5cc93eef46f40e0e3d5d6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/work_from_roam-client/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
