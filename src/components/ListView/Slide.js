e1aaf20995c83ebb43042ece44e177824f72ca14
chardbres <richard.m.bresnahan@gmail.com>
2020-04-06 16:34:01 -0400

    Adjusted styling in Slide.js to include styled rating bars. Added cute buffalo as placeholder image."

--- src/components/ListView/Slide.js
+++ src/components/ListView/Slide.js
@@ -3,12 +3,19 @@ import React from 'react'
 import { css, jsx } from '@emotion/core'
 import img from './cute_buffalo.jpg'
 
+<<<<<<< HEAD
 // Custon component imports
 import ScaleRating from '../WorkSpace/ScaleRating'
 
 const Slide = ({ content, width, activeIndex }) => (
   <div
     css={css`
+=======
+const Slide = ({ content, width, activeIndex }) => (
+  <div
+    css={css`
+      border: 1px solid red;
+>>>>>>> 2e0ba2ae55ad22cb0d9ac07819016ee83aec2b60
       display: flex;
       flex-direction: column;
       height: 100%;
@@ -16,6 +23,7 @@ const Slide = ({ content, width, activeIndex }) => (
       position: inline-block;
       `}
   >
+<<<<<<< HEAD
     <div
       css={css`
         width: 100%;
@@ -27,6 +35,13 @@ const Slide = ({ content, width, activeIndex }) => (
         `} />
     <ScaleRating data={content}/>
     <p>{content.place_id}</p>
+=======
+    <p>{content.place_id}</p>
+    <p>{content.avg_noise}</p>
+    <p>{content.id}</p>
+    <p>{width}</p>
+    <p>{activeIndex}</p>
+>>>>>>> 2e0ba2ae55ad22cb0d9ac07819016ee83aec2b60
   </div>
 )
 
