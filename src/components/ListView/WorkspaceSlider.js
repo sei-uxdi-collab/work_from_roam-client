2e0ba2ae55ad22cb0d9ac07819016ee83aec2b60
chardbres <richard.m.bresnahan@gmail.com>
2020-04-06 13:44:35 -0400

    Completed initial ListView functionality. Had to fiddle with styling quite a bit to get the increment/decrement arrows outside of the workspace detail view, and to store the proper width of each slide element.

--- src/components/ListView/WorkspaceSlider.js
+++ src/components/ListView/WorkspaceSlider.js
@@ -2,10 +2,10 @@ import React, { useState } from 'react'
 import styled from '@emotion/styled'
 
 const WorkspaceSlider = styled.div`
-  transform: translateX(-${props => props.translatee} px);
+  transform: translateX(-${props => props.translate}px);
   transition: transform ease-out ${props => props.transition}s;
   height: 100%;
-  width: 100%;
+  width: ${props => props.width}px;
   display: flex;
 `
 
