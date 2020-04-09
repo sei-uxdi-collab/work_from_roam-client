f52cf382c5b4799964d0124a4e17ae4113108dae
reginapizza <reginaelyse@gmail.com>
2020-04-09 18:51:31 -0400

    Destructured code out of header
    Split code either between unauthenticatedOptions or authenticatedOptions
    based on if user is true, that way Header is basically just a switch
    statement.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,46 +1,79 @@
 import React, { Fragment } from 'react'
-import { Row, Col } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom'
 // import Nav from 'react-bootstrap/Nav'
 // import Navbar from 'react-bootstrap/Navbar'
-
-
 import './Header.scss'
 
-const authenticatedOptions = (
+const userBanner = ( user ) => (
   <Fragment>
-    <Row>
-      <Col>
-        <Link to='/'>
-          <img src="close-x-white.png" className="icon" alt="close"/>
-        </Link>
-      </Col>
-    </Row>
-    <Row>
-      <img src="your-favorites-heart-icon.png" className="icon" alt="Your Favorites"/> <a href="/">Your Favorites</a>
-    </Row>
-    <Row>
-      <img src="my-reviews-icon.png" className="icon" alt="My Reviews"/> <a href="/">My Reviews</a>
-    </Row>
-    <Row>
-      <img src="top-rated-star-icon.png" className="icon" alt="Top Rated"/> <a href="/">Top Rated</a>
-    </Row>
-    <Row>
-      <img src="info-icon.png" className="icon" alt="info"/> <a href="/">Info</a>
-    </Row>
-    <Row>
-      <img src="info-icon.png" className="icon" alt="Change Password"/> <a href="/">Change Password</a>
-      <a href="#sign-out">Sign Out</a>
-    </Row>
+    <img src="profile-pic-placeholder.png" className="prof-pic" alt="profile pic" />
+    <span style={{ color: 'black' }} className="">Welcome back, {user.email}</span>}
   </Fragment>
 )
 
+const authenticatedOptions = user => (
+  <Fragment>
+  <Row>
+    <Link to='/' className="p-0">
+      <img src="close-x-white.png" className="icon" alt="close"/>
+    </Link>
+    <div>
+      { userBanner(user) }
+    </div>
+  </Row>
+    <Row>
+      <div>
+        <img src="your-favorites-heart-icon.png" className="icon" alt="Your Favorites"/>
+      </div>
+      <div>
+        <a href="/">Your Favorites</a>
+      </div>
+    </Row>
+    <Row>
+      <div>
+        <img src="my-reviews-icon.png" className="icon" alt="My Reviews"/>
+      </div>
+      <div>
+        <a href="/">My Reviews</a>
+      </div>
+    </Row>
+    <Row>
+      <div>
+        <img src="top-rated-star-icon.png" className="icon" alt="Top Rated"/>
+      </div>
+      <div>
+        <a href="/">Top Rated</a>
+      </div>
+    </Row>
+    <Row>
+      <div>
+        <img src="info-icon.png" className="icon" alt="info"/>
+      </div>
+      <div>
+        <a href="/">Info</a>
+      </div>
+    </Row>
+    <Row>
+      <div>
+        <img src="info-icon.png" className="icon" alt="Change Password"/>
+      </div>
+      <div>
+        <a href="#change-password">Change Password</a>
+      </div>
+      <div>
+        <a href="#sign-out">Sign Out</a>
+      </div>
+    </Row>
+  </Fragment>
+)
 const unauthenticatedOptions = (
   <Fragment>
     <Row>
+      <Link to='/' className="p-0">
+        <img src="close-x-white.png" className="icon" alt="close"/>
+      </Link>
       <a href="#sign-up">Sign Up</a>
-    </Row>
-    <Row>
       <a href="#sign-in">Sign In</a>
     </Row>
     <Row>
@@ -48,26 +81,13 @@ const unauthenticatedOptions = (
     </Row>
   </Fragment>
 )
-
 const Header = ({ user }) => (
   <div className="header" collapseOnSelect fixed="top">
-    <Row>
-      <Col className="p-0">
-        <Link to='/'>
-          <img src="close-x-white.png" className="icon" alt="close"/>
-        </Link>
-      </Col>
-      <Col>
-        { user && <span style={{ color: 'black' }} className="">Welcome, {user.email}</span>}
-      </Col>
-    </Row>
-    <br />
     <div id="">
-      <div className="">
-        { user ? authenticatedOptions : unauthenticatedOptions }
+      <div>
+        { user ? authenticatedOptions(user) : unauthenticatedOptions }
       </div>
     </div>
   </div>
 )
-
 export default Header
