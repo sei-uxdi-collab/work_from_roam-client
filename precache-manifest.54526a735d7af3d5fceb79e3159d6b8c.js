db5c16a7bc929b93adc0689da469e007fa9b81aa
asooge <asooge@gmail.com>
2020-04-09 17:32:17 -0400

    Updates

---
+++ precache-manifest.54526a735d7af3d5fceb79e3159d6b8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2242a579b1fa469142368a4c35dec9f",
+    "url": "/work_from_roam-client/index.html"
+  },
+  {
+    "revision": "9920ea723fedcdcbd928",
+    "url": "/work_from_roam-client/static/css/main.a47b2f95.chunk.css"
+  },
+  {
+    "revision": "78d1852967e26e240e5f",
+    "url": "/work_from_roam-client/static/js/2.c4e84d91.chunk.js"
+  },
+  {
+    "revision": "2b8cc04d3b239d52c1e995484cbcd9e3",
+    "url": "/work_from_roam-client/static/js/2.c4e84d91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9920ea723fedcdcbd928",
+    "url": "/work_from_roam-client/static/js/main.9640a9fc.chunk.js"
+  },
+  {
+    "revision": "691df9043ad91b2ff65d",
+    "url": "/work_from_roam-client/static/js/runtime-main.632891d8.js"
+  },
+  {
+    "revision": "b1f18555a8008168965bb2bb71773fd2",
+    "url": "/work_from_roam-client/static/media/cute_buffalo.b1f18555.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/work_from_roam-client/precache-manifest.542c6e143ba5cc93eef46f40e0e3d5d6.js"
+  "/work_from_roam-client/precache-manifest.54526a735d7af3d5fceb79e3159d6b8c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/work_from_roam-client/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
