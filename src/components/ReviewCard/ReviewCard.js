626897e5c3a686d446166cdd3337d21cccc089c0
Mahelet.K <maheletkassa1@gmail.com>
2020-04-19 22:23:37 -0400

    -Refactored the code -added scss to the class selector to align to the figma wireframe

--- src/components/ReviewCard/ReviewCard.js
+++ src/components/ReviewCard/ReviewCard.js
@@ -1,51 +1,31 @@
 import React from "react"
 import { StarRating } from '../Review/StarsRating'
 import './ReviewCard.scss'
-// import image from '..././../public/image_not_found.png'
-
 
 class ReviewCard extends React.Component {
   constructor() {
     super()
-    this.state = {
-      name: '',
-      rating: 3,
-      note:'',
-      distance: '',
-      hours: false,
-      address: '',
-      image_url: ''
-
-    }
 
   }
+
 render() {
+
   const {value} = this.props;
 
     return (
-       <div>
+       <div className= 'Border'>
           <div className='header-container'>
-            <h1>Your Review</h1>
+          <h1>{this.props.review && this.props.review.work_space.name}</h1>
             <StarRating
-             value={value}
+             value={this.props.review && this.props.review.rating}
              emptyStarColor='#FFFF00'
              editing={false}
             />
-            <h2>Date of Review</h2>
+            <p>{this.props.review && this.props.review.work_space.address}</p>
           </div>
           <div className="body-container">
-            <h1>{this.props.review && this.props.review.work_space.name}</h1>
-            <StarRating
-             value={this.props.review && this.props.review.work_space.rating}
-             emptyStarColor='#4775FF'
-             editing={false}
-            />
-            <p>{this.props.review && this.props.review.work_space.address}</p>
-            <div className="photo">
-            <img src={this.props.review && this.props.review.work_space.photo}/>
-            </div>
             <p>{this.props.review && this.props.review.note}</p>
-
+            <h2>Date of Review</h2>
           </div>
         </div>
     );
