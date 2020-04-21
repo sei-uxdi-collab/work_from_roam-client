2264bb33251886170069b318c655d97815f47a35
Mahelet.K <maheletkassa1@gmail.com>
2020-04-20 21:43:12 -0400

    Updated App.js file

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react'
 import { Route } from 'react-router-dom'
+
 import AutoAlert from '../AutoAlert/AutoAlert.js'
 import Search from '../Search/Search'
 import GoogleMap from '../GoogleMap/GoogleMap'
@@ -14,7 +15,10 @@ import WorkSpace from '../WorkSpace/WorkSpace'
 import WorkSpaceCreate from '../WorkSpace/WorkSpaceCreate.js'
 import ListView from '../ListView/ListView'
 import WorkspaceFilter from '../WorkspaceFilter/WorkspaceFilter'
+
 import './App.scss'
+
+
 class App extends React.Component {
   constructor() {
     super()
@@ -33,24 +37,32 @@ class App extends React.Component {
       alerts: [],
       filteredWorkspaces: [],
     }
+
     // Allows the WorkspaceFilter component to update filteredWorkspaces[]
     this.filteredWorkspaces = this.filterWorkspaces.bind(this)
   }
+
   // Function bound to filteredWorkspaces state, passed in to the WorkspaceFilter component as props
   filterWorkspaces = workspaces => {
     this.setState({ filteredWorkspaces: workspaces})
   }
   // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
   setUser = user => this.setState({ user })
+
   clearUser = () => this.setState({ user: null })
+
   alert = ({ heading, message, variant, image }) => {
     this.setState({ alerts: [...this.state.alerts, { heading, message, variant, image }] })
   }
+
   render() {
     const { alerts, user } = this.state
+
     return (
 <div>
 <Fragment>
+
         {alerts.map((alert, index) => (
             <AutoAlert
               key={index}
@@ -60,6 +72,7 @@ class App extends React.Component {
               image={alert.image}
             />
           ))}
+
         <Route path='/work_spaces/:id/create-review'>
           <ReviewCreate
             user={user}
@@ -72,27 +85,34 @@ class App extends React.Component {
             setApp={this.setState.bind(this)}
           />
         </Route>
+
         <Route path='/workspace' render={() => (
            <WorkSpace
               data={this.state.currentWorkspace}
               placeData={this.state.placeData}
             />
          )} />
+
         <Route path='/sign-up' render={() => (
            <SignUp alert={this.alert} setUser={this.setUser} />
          )} />
+
          <Route path='/sign-in' render={() => (
             <SignIn user={user} alert={this.alert} setUser={this.setUser} />
           )} />
+
           <Route user={user} path='/change-password' render={() => (
               <ChangePassword user={user} alert={this.alert} />
             )} />
+
           <Route user={user} path='/sign-out' render={() => (
               <SignOut clearUser={this.clearUser} alert={this.alert} user={user} />
             )} />
+
           <Route user={user} path='/nav' render={() => (
               <Header clearUser={this.clearUser} user={user} />
             )} />
+
             <Route user={user} path='/create-workspace' render={() => (
             <WorkSpaceCreate
               user={user}
@@ -102,6 +122,9 @@ class App extends React.Component {
               setApp={this.setState.bind(this)}
             />
           )} />
+
+
+
         <Route path='/'>
           <div className="App">
             <div className='search-group'>
@@ -127,7 +150,10 @@ class App extends React.Component {
         </Route>
       </Fragment>
       </div>
+
+
     )
   }
 }
-export default App
+
+export default 
