ef88cf4691eb69810b0770ab4f7a1903fc10ad3a
GMorse19 <malorse@comcast.net>
2020-05-05 00:15:59 -0400

    Added new alert component for alt alert messages

---
+++ src/components/BarAlert/BarAlert.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import Alert from 'react-bootstrap/Alert'
+
+import './BarAlert.scss'
+
+class BarAlert extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      show: true
+    }
+  }
+
+  componentDidMount () {
+    this.timer = setInterval(() => {
+      this.setState({ show: false })
+    }, 3000)
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.timer)
+  }
+
+  handleClose = () => this.setState({ show: false })
+
+  render () {
+    const { variant, message } = this.props
+    return (
+      <Alert
+        style={{
+          borderRadius: '0px',
+          width: '100vw',
+          textAlign: 'center',
+          color: 'black',
+          backgroundColor: '#a4ff2e',
+          fonSize: '11px',
+          fontFamily: 'Roboto',
+          fontWeight: '500',
+          top: '3%' }}
+        show={this.state.show}
+        variant={variant}
+        onClose={this.handleClose}
+        onClick={this.handleClose}
+        className={variant}
+      >
+        <div>{message}</div>
+      </Alert>
+    )
+  }
+}
+
+export default BarAlert
