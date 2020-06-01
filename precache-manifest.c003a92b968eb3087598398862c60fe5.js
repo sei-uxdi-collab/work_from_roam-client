02f370a9f989b552efd8ca119f2d0404bfbd3ccd
asooge <asooge@gmail.com>
2020-06-01 16:18:37 -0400

    Updates

---
+++ precache-manifest.c003a92b968eb3087598398862c60fe5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f630ad1a3576dbc74d80513529384c05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52230fdc2dba7193fb90",
+    "url": "/static/css/2.240942cf.chunk.css"
+  },
+  {
+    "revision": "16af2432e4c6f8b446e0",
+    "url": "/static/css/main.9163b674.chunk.css"
+  },
+  {
+    "revision": "52230fdc2dba7193fb90",
+    "url": "/static/js/2.b9378638.chunk.js"
+  },
+  {
+    "revision": "30dd72af26da10067e6d09d1bf603e0b",
+    "url": "/static/js/2.b9378638.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16af2432e4c6f8b446e0",
+    "url": "/static/js/main.dcfcab83.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "8bd96d7b2cd0a782cc546b04b0a83b94",
+    "url": "/static/media/Header.8bd96d7b.svg"
+  },
+  {
+    "revision": "23b74083570c0d1c9d5e812f43d16105",
+    "url": "/static/media/glasses-icon.23b74083.svg"
+  },
+  {
+    "revision": "c09232999c77b9d8260f1cb571a5ba0c",
+    "url": "/static/media/img1.c0923299.svg"
+  },
+  {
+    "revision": "915d76d0a2361215b61b3bc89bb7805a",
+    "url": "/static/media/img2.915d76d0.svg"
+  },
+  {
+    "revision": "e22d2fe857a47e70cc75b8ee39b4e0e0",
+    "url": "/static/media/img3.e22d2fe8.svg"
+  },
+  {
+    "revision": "75ee846a6ec76463983ccdfbe109e4f3",
+    "url": "/static/media/img4.75ee846a.svg"
+  }
+]);
