9b1d75ab0e4e0bda13ed4e7334c75ee141398ac0
GMorse19 <malorse@comcast.net>
2020-07-23 16:05:52 -0400

    Refactored and moved images to public folder

--- src/components/HappySlider/smileys.js
+++ src/components/HappySlider/smileys.js
@@ -1,11 +1,8 @@
-// Relative path does not work for css Variable
-// import images into a variable
-import smiley1 from './Smileys/smiley1.svg'
-import smiley2 from './Smileys/smiley2.svg'
-import smiley3 from './Smileys/smiley3.svg'
-import smiley4 from './Smileys/smiley4.svg'
-import smiley5 from './Smileys/smiley5.svg'
-
 // Create an array for images
 // Each index coinciding with image number
-export const smileys = ['', smiley1, smiley2, smiley3, smiley4, smiley5]
+export const smileys = ['',
+  'Smileys/smiley1.svg',
+  'Smileys/smiley2.svg',
+  'Smileys/smiley3.svg',
+  'Smileys/smiley4.svg',
+  'Smileys/smiley5.svg']
