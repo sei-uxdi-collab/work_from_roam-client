a642dd5bc0c618013dae15a71782a1abed5ede4b
GMorse19 <malorse@comcast.net>
2020-08-27 22:58:55 -0400

    Sign up validation messsages

---
+++ src/components/SignUp/signUpMessages.js
@@ -0,0 +1,12 @@
+export default {
+  email: 'Must be a valid email.',
+  emailAvail: 'This email already has an account',
+  username: 'Username must contain 6 characters.',
+  usernameTaken: 'User name is taken. Please Choose another.',
+  passwordLength: 'Password must be 8 characters long.',
+  passwordCapital: 'Password must contain one Uppercase letter.',
+  passwordLower: 'Password must contain one Lowercase letter.',
+  passwordSpecial: 'Password must contain one special character.',
+  passwordNumber: 'Password must contain one number.',
+  passwordConfirmation: 'Passwords do not match'
+}
