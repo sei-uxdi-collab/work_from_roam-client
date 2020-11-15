979b1067e1e38eb7bdec21f6ace69abc6924d6b7
asooge <asooge@gmail.com>
2020-11-15 14:34:46 -0500

    refactor Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,7 @@
-import React, { Fragment, useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Row } from 'react-bootstrap'
 import { showUser } from '../../api/auth'
 import { Link, withRouter } from 'react-router-dom'
-// import Nav from 'react-bootstrap/Nav'
-// import Navbar from 'react-bootstrap/Navbar'
 import './Header.scss'
 import Info from "../Info/Info";
 import TopRated from "../TopRated/TopRated";
@@ -45,8 +43,9 @@ const Header = ({ user, userLocation, allData, setApp, setUser, google, map, his
       signUp: false,
       [section]: newState
     })
-
   }
+
+  const expandByKey = (key) => setExpanded(prevState => ({ ...prevState, [key]: !prevState[key] }))
 
   const showWorkspace = (id, placeId) => {
     const callback = placeData => {
@@ -61,58 +60,68 @@ const Header = ({ user, userLocation, allData, setApp, setUser, google, map, his
     history.push('/workspace')
   }
 
-  const userBanner = (
-    <Fragment>
-      <div className="d-flex mb-2" style={{ width: "100%"}}>
-        <div className="user-avatar">
-          <a href="#avatar">
-            { user && user.avatar ? <img src={avatar(user.avatar)} className="prof-pic" alt="profile pic"/> : <img src={avatar(25)} className="prof-pic" alt="profile pic" /> }
-          </a>
-        </div>
-        <div className="ml-4">
-          <Row>
-            <div><span className="welcome" title={`Welcome back, ${user && (user.username || user.email)}`}>Welcome back, {user && (user.username || user.email)}</span></div>
-          </Row>
-        </div>
-      </div>
-    </Fragment>
-  )
+  return (
+    <div className="header" fixed="top">
+      <Row>
+        <Link to='/' className="p-0">
+          <img src="close-x-white.png" className="close-x-white" alt="close"/>
+        </Link>
+        {user ? (
+          <div className="d-flex mb-2" style={{ width: "100%"}}>
+            <div className="user-avatar">
+              <a href="#avatar">
+                { user && user.avatar ? <img src={avatar(user.avatar)} className="prof-pic" alt="profile pic"/> : <img src={avatar(25)} className="prof-pic" alt="profile pic" /> }
+              </a>
+            </div>
+            <div className="ml-4">
+              <Row>
+                <div><span className="welcome" title={`Welcome back, ${user && (user.username || user.email)}`}>Welcome back, {user && (user.username || user.email)}</span></div>
+              </Row>
+            </div>
+          </div>
+        ) : (
+          <HeaderAuthOptions
+            barAlert={barAlert}
+            setUser={setUser}
+            toggleSignIn={() => toggleExpand('signIn')}
+            toggleSignUp={() => toggleExpand('signUp')}
+            isSignInOpen={expanded.signIn}
+            isSignUpOpen={expanded.signUp}
+          />
+        )}
+      </Row>
 
-  const authenticatedOptions = (
-    <Fragment>
-    <Row>
-      <Link to='/' className="p-0">
-        <img src="close-x-white.png" className="close-x-white" alt="close"/>
-      </Link>
-      <div>
-        { userBanner }
-      </div>
-    </Row>
-      <Row>
-        <div>
-          <img src="your-favorites-heart-icon.png" className="icon" alt="Your Favorites"/>
-        </div>
-        <MyFavorites
-          user={user}
-          userLocation={userLocation}
-          isExpanded={expanded.favorites}
-          toggleExpand={() => toggleExpand('favorites')}
-          showWorkspace={showWorkspace}
-        />
-      </Row>
-      <Row>
-        <div>
-          <img src="my-reviews-icon.png" className="icon" alt="My Reviews"/>
-        </div>
-        <MyReviews
-          user={user}
-          isExpanded={expanded.reviews}
-          toggleExpand={() => toggleExpand('reviews')}
-          allData={allData}
-          setApp={setApp}
-          showWorkspace={showWorkspace}
-        />
-      </Row>
+      {user && (
+        <Row>
+          <div>
+            <img src="your-favorites-heart-icon.png" className="icon" alt="Your Favorites"/>
+          </div>
+          <MyFavorites
+            user={user}
+            userLocation={userLocation}
+            isExpanded={expanded.favorites}
+            toggleExpand={() => toggleExpand('favorites')}
+            showWorkspace={showWorkspace}
+          />
+        </Row>
+      )}
+
+      {user && (
+        <Row>
+          <div>
+            <img src="my-reviews-icon.png" className="icon" alt="My Reviews"/>
+          </div>
+          <MyReviews
+            user={user}
+            isExpanded={expanded.reviews}
+            toggleExpand={() => toggleExpand('reviews')}
+            allData={allData}
+            setApp={setApp}
+            showWorkspace={showWorkspace}
+          />
+        </Row>
+      )}
+
       <Row>
         <div>
           <img src="top-rated-star-icon.png" className="icon" alt="Top Rated"/>
@@ -125,70 +134,33 @@ const Header = ({ user, userLocation, allData, setApp, setUser, google, map, his
           showWorkspace={showWorkspace}
         />
       </Row>
-      <Row>
-        <div>
-          <img src="settings-icon.svg" className="icon" alt="Settings"/>
-        </div>
-        <Settings isExpanded={expanded.settings} toggleExpand={() => toggleExpand('settings')}/>
-      </Row>
-      <div>
-        <a className="btn sign-out" href="#sign-out" role="button">Sign Out</a>
-      </div>
-    </Fragment>
-  )
 
-  const unauthenticatedOptions = (
-    <Fragment>
-      <Row>
-        <Link to='/' className="p-0">
-          <img src="close-x-white.png" className="close-x-white" alt="close"/>
-        </Link>
-        <HeaderAuthOptions
-          barAlert={barAlert}
-          setUser={setUser}
-          toggleSignIn={() => toggleExpand('signIn')}
-          toggleSignUp={() => toggleExpand('signUp')}
-          isSignInOpen={expanded.signIn}
-          isSignUpOpen={expanded.signUp}
-        />
-      </Row>
-      <Row>
-        <div>
-          <img src="top-rated-star-icon.png" className="icon" alt="Top Rated"/>
-        </div>
-        <div className="list-item">
-          <TopRated
-            user={user}
-            userLocation={userLocation}
-            isExpanded={expanded.topRated}
-            toggleExpand={() => toggleExpand('topRated')}
-            showWorkspace={showWorkspace}
-          />
-        </div>
-      </Row>
-      <Row>
-        <div>
-          <img src="info-icon.png" className="icon" alt="info"/>
-        </div>
-        <div className="list-item">
+      {user ? (
+        <Row>
+          <div>
+            <img src="settings-icon.svg" className="icon" alt="Settings"/>
+          </div>
+          <Settings isExpanded={expanded.settings} toggleExpand={() => toggleExpand('settings')}/>
+        </Row>
+      ) : (
+        <Row>
+          <div>
+            <img src="info-icon.png" className="icon" alt="info"/>
+          </div>
           <Info
             title="Info"
             content="This is our app info: It's great! That's all you need to know."
             isExpanded={expanded.info}
-            toggleExpand={() => toggleExpand('info')}
+            toggleExpand={() => expandByKey('info')}
           />
-        </div>
-      </Row>
-    </Fragment>
-  )
+        </Row>
+      )}
 
-  return (
-    <div className="header" fixed="top">
-      <div id="">
+      {user && (
         <div>
-          { user ? authenticatedOptions : unauthenticatedOptions }
+          <a className="btn sign-out" href="#sign-out" role="button">Sign Out</a>
         </div>
-      </div>
+      )}
     </div>
   )
 }
